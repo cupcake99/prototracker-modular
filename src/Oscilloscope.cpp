e7afe618ebb973d510030ecc126a085325229c30
cupcake99 <beyonda@icloud.com>
2018-06-05 21:48:13 +0000

    Removed flat-line display in scopes when muted.
    Also killed some pointless whitespace.

--- src/Oscilloscope.cpp
+++ src/Oscilloscope.cpp
@@ -12,7 +12,7 @@ Oscilloscope::Oscilloscope(EditorState& editorState, IPlayer& player, int channe
 {
 }
 
-	
+
 Oscilloscope::~Oscilloscope()
 {
 }
@@ -30,11 +30,11 @@ void Oscilloscope::onDraw(Renderer& renderer, const SDL_Rect& area)
 {
 	setDirty(false);
 	renderer.renderBackground(area);
-	
+
 	SDL_Point points[area.w];
 	SDL_Rect block = {};
 	SDL_Rect textPos = {};
-	
+
 	if (mPlayer.getTrackState(mChannel).enabled)
 	{
 		for (int x = 0 ; x < area.w ; ++x)
@@ -42,16 +42,16 @@ void Oscilloscope::onDraw(Renderer& renderer, const SDL_Rect& area)
 			int bufferPos = mBufferLength * x / area.w;
 			const Sample16& sample = mBuffer[(bufferPos + mOscilloscopePos) % mBufferLength];
 			int y = -(sample.left + sample.right) * area.h / IOscillator::oscillatorResolution / 2 + area.h / 2;
-			
+
 			if (y >= area.h)
 				y = area.h - 1;
 			else if (y < 0)
 				y = 0;
-			
+
 			points[x].x = area.x + x;
 			points[x].y = area.y + y;
 		}
-		
+
 		renderer.renderPoints(points, area.w, Color());
 	}
 	else
@@ -61,11 +61,11 @@ void Oscilloscope::onDraw(Renderer& renderer, const SDL_Rect& area)
 			points[x].x = area.x + x;
 			points[x].y = area.h / 2 + 2;
 		}
-		
+
 		block = {area.x + 10, area.h / 2 - 2, 35, 9};
 		textPos = {block.x + 2, block.y + 1, 33, 8};
-		
-		renderer.renderPoints(points, area.w, Color());
+
+//		renderer.renderPoints(points, area.w, Color());
 		renderer.renderRect(block, Color(255,0,0));
 		renderer.renderText(textPos, Color(), "mute");
 	}
@@ -79,7 +79,7 @@ bool Oscilloscope::onEvent(SDL_Event& event)
 		mPlayer.getTrackState(mChannel).enabled ^= true;
 		return true;
 	}
-	
+
 	return false;
 }
 
@@ -87,7 +87,7 @@ bool Oscilloscope::onEvent(SDL_Event& event)
 void Oscilloscope::onListenableChange(Listenable *listenable)
 {
 	setDirty(true);
-	
+
 	// Assumes this will always be called by mOscillatorsProbePos->notify()
 	mOscilloscopePos = *static_cast<Value*>(listenable);
 }
